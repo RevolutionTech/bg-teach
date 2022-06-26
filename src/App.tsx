2cb984ee603aa20e4b03720ae23be1b1b3dd640f
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-06-25 23:34:17 -0700

    refactor: remove unused prop on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
 import * as React from "react";
 
-interface Props {
-  name: string;
-}
-
-class App extends React.Component<Props> {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1 className="text-4xl text-white bg-black">Hello {name}</h1>
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+    <h1 className="text-4xl text-white bg-black">Hello world!</h1>
+  </>
+);
 
 export default App;
