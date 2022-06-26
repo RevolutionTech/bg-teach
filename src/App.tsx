fb6239c2114a3de845126c0108d534b29b845fc7
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-06-25 22:48:50 -0700

    style: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-
-import * as React from 'react';
+import * as React from "react";
 
 interface Props {
-   name:
-    string
+  name: string;
 }
 
 class App extends React.Component<Props> {
@@ -11,9 +9,7 @@ class App extends React.Component<Props> {
     const { name } = this.props;
     return (
       <>
-        <h1 className="text-4xl text-white bg-black">
-          Hello {name}
-        </h1>
+        <h1 className="text-4xl text-white bg-black">Hello {name}</h1>
       </>
     );
   }
