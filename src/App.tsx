1f0df5b99dff4eb0719ff653c85c0803b4193342
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-06-26 20:51:59 -0700

    refactor: encapsulate useMarkdownString hook into Teach component

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,13 @@ import Teach from "./Teach";
 import TragedyLooper from "url:./teaches/tragedy-looper.md";
 import Tzolkin from "url:./teaches/tzolkin.md";
 import WesternLegends from "url:./teaches/western-legends.md";
-import useMarkdownString from "./useMarkdownString";
 
-const App = () => {
-  const westernLegends = useMarkdownString(WesternLegends);
-  const tzolkin = useMarkdownString(Tzolkin);
-  const tragedyLooper = useMarkdownString(TragedyLooper);
-
-  return (
-    <>
-      <Teach markdown={westernLegends} />
-      <Teach markdown={tzolkin} />
-      <Teach markdown={tragedyLooper} />
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Teach notes={WesternLegends} />
+    <Teach notes={Tzolkin} />
+    <Teach notes={TragedyLooper} />
+  </>
+);
 
 export default App;
--- src/Teach.tsx
+++ src/Teach.tsx
@@ -2,24 +2,37 @@ import * as React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+const useMarkdownString = (url: string) => {
+  const [markdown, setMarkdown] = React.useState<string>("");
+  React.useEffect(() => {
+    fetch(url)
+      .then((res) => res.text())
+      .then((md) => setMarkdown(md));
+  }, [url, setMarkdown]);
+  return markdown;
+};
+
 interface TeachProps {
-  markdown: string;
+  notes: string;
 }
 
-const Teach = (props: TeachProps) => (
-  <ReactMarkdown
-    components={{
-      input: ({
-        type,
-        ...props
-      }: React.InputHTMLAttributes<HTMLInputElement>) => (
-        <input type={type} {...(type === "checkbox" ? {} : props)} />
-      ),
-    }}
-    rehypePlugins={[remarkGfm]}
-  >
-    {props.markdown}
-  </ReactMarkdown>
-);
+const Teach = (props: TeachProps) => {
+  const markdown = useMarkdownString(props.notes);
+  return (
+    <ReactMarkdown
+      components={{
+        input: ({
+          type,
+          ...props
+        }: React.InputHTMLAttributes<HTMLInputElement>) => (
+          <input type={type} {...(type === "checkbox" ? {} : props)} />
+        ),
+      }}
+      rehypePlugins={[remarkGfm]}
+    >
+      {markdown}
+    </ReactMarkdown>
+  );
+};
 
 export default Teach;
