63ff11346a8a81c817c9d18c6aedeee57c7f7fb9
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-06-26 18:52:32 -0700

    refactor: load markdown from file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import * as React from "react";
 
 import Teach from "./Teach";
+import WesternLegends from "url:./teaches/western-legends.md";
+import useMarkdownString from "./useMarkdownString";
 
-const WESTERN_LEGENDS = `
-# Western Legends
-
-- [ ] Theme & Vibe
-  - [ ] Sandbox
-  - [ ] Outlaws: rob bank, rustle cattle, steal
-  - [ ] Marshals: fight bandits, wrangle cattle, arrest Wanted players
-- [ ] Objective: most legendary points (LP)
-- [ ] Game end: penultimate round when 15/20/25 LPs reached
-- [ ] Poker cards
-  - [ ] Used directly for effects, used in fights, and playing poker
-  - [ ] Each card has action, reaction, or bonus
-`;
-
-const App = () => (
-  <>
-    <Teach markdown={WESTERN_LEGENDS} />
-  </>
-);
+const App = () => {
+  const markdown = useMarkdownString(WesternLegends);
+  return (
+    <>
+      <Teach markdown={markdown} />
+    </>
+  );
+};
 
 export default App;
---
+++ src/teaches/western-legends.md
@@ -0,0 +1,11 @@
+# Western Legends
+
+- [ ] Theme & Vibe
+  - [ ] Sandbox
+  - [ ] Outlaws: rob bank, rustle cattle, steal
+  - [ ] Marshals: fight bandits, wrangle cattle, arrest Wanted players
+- [ ] Objective: most legendary points (LP)
+- [ ] Game end: penultimate round when 15/20/25 LPs reached
+- [ ] Poker cards
+  - [ ] Used directly for effects, used in fights, and playing poker
+  - [ ] Each card has action, reaction, or bonus
